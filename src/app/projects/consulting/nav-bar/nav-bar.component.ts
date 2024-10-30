9a5debd2f9d04189792f5a2b0d0ee8da8b5ac2b3
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-10-29 20:06:54 -0400

    fixed hamburger

--- src/app/projects/consulting/nav-bar/nav-bar.component.ts
+++ src/app/projects/consulting/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
