cc2d37f73bdc1363b06e579b0bafbcad5725b772
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-11-03 21:00:16 -0500

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HeadingComponent } from './heading/heading.component';
 import { ServiceCardComponent } from './heading/service-card/service-card.component';
 import { ContactSectionComponent } from './contact-section/contact-section.component';
 import { ExperienceComponent } from './experience/experience.component';
-import { SERVICES } from './services/service.data';
 import { DomSanitizer } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
 import { CompanyCarouselComponent } from './company-carousel/company-carousel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
@@ -22,8 +20,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 })
 export class AppComponent {
   constructor(
-    public sanitizer: DomSanitizer,
-    private http: HttpClient
+    public sanitizer: DomSanitizer
   ){
   }
 
