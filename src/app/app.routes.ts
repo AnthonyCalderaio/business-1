80da70347ff8adc3434a70e1e36b4e778db62f00
Anthony Calderaio <anthonycalderaio@gmail.com>
2024-12-13 18:29:13 -0500

    created page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExpertiseHighlightsComponent } from './pages/expertise-highlights/expertise-highlights.component';
 import { BlogComponent } from './pages/blog/blog.component';
+import { DownloadsComponent } from './downloads/downloads.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'expertise', component: ExpertiseHighlightsComponent },
+    { path: 'downloads', component: DownloadsComponent },
     { path: 'blog', component: BlogComponent}
 ];
